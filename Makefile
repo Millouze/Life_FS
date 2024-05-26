dbb5af437d6e9f31e71b72c5e7a0b7b8ee1e0969
Florent Bordas <florentbordas18@gmail.com>
2024-05-26 17:28:23 +0200

    fix: unit_test start_insertion fonctionnel

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 obj-m += ouichefs.o 
 ouichefs-objs := fs.o super.o inode.o file.o dir.o common.o read.o write.o ioctl.o defragmentation.o
 
-KERNELDIR ?= /home/tara/linux-6.5.7
+KERNELDIR ?= /home/floflo/PNL/linux-6.5.7
 PWD := $(shell pwd)
 
 all:
 	make -C $(KERNELDIR) M=$(PWD) modules
-	cp ouichefs.ko /home/tara/master/S2/PNL/share/
+	cp ouichefs.ko /home/floflo/PNL/share/
 
 debug:
 	make -C $(KERNELDIR) M=$(PWD) ccflags-y+="-DDEBUG -g" modules
--- usercode/unit_test.c
+++ usercode/unit_test.c
@@ -1,4 +1,5 @@
 #include "utils.h"
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,6 +8,8 @@
 
 int start_insertion(int fd, char *rd_str, size_t str_sz)
 {
+	int a = 0;
+	int b = 0;
 	char *res_str = malloc(str_sz);
 	memset(res_str, 0, str_sz);
 
@@ -16,11 +19,10 @@ int start_insertion(int fd, char *rd_str, size_t str_sz)
 		dprintf(STDOUT_FILENO, "START_INSERTION : write error \n");
 		exit(EXIT_FAILURE);
 	}
-	printf("rd_str : %s \n", rd_str);
 	lseek(fd, 0, SEEK_SET);
-	if ((read(fd, res_str, str_sz) != str_sz) &&
-	    strncmp(rd_str, res_str, str_sz) == 0) {
-		printf("res_str : %s \n", res_str);
+	a = read(fd, res_str, str_sz);
+	b = strncmp(rd_str, res_str, str_sz);
+	if ((a == str_sz) && (b == 0)) {
 		dprintf(STDOUT_FILENO,
 			"First Write Successful in start_insertion \n");
 	} else {
@@ -40,24 +42,11 @@ int start_insertion(int fd, char *rd_str, size_t str_sz)
 	}
 
 	// Positionnement nouveau bloc alloue verif ecriture
-	lseek(fd, 4097, SEEK_SET);
+	lseek(fd, 0, SEEK_SET);
 
-	if ((read(fd, res_str, str_sz) != str_sz) &&
-	    (strncmp(nw_str, res_str, str_sz) == 0)) {
-		goto TEST_BLK_SPLIT;
-	} else {
-		dprintf(STDOUT_FILENO,
-			"Second Write Failed in start_insertion \n");
-		return EXIT_FAILURE;
-	}
-
-	// Positionnement bloc de SPLIT
-	lseek(fd, 8193, SEEK_SET);
-
-TEST_BLK_SPLIT:
-
-	if ((read(fd, res_str, str_sz) != 0) &&
-	    (strncmp(res_str, rd_str, str_sz) == 0)) {
+	a = read(fd, res_str, str_sz);
+	b = strncmp(nw_str, res_str, str_sz);
+	if ((a == str_sz) && (b == 0)) {
 		printf("START INSERTION : ");
 		printf("\033[0;32m");
 		printf("SUCCESS \n");
@@ -66,24 +55,27 @@ TEST_BLK_SPLIT:
 	} else {
 		printf("START INSERTION : ");
 		printf("\033[0;31m");
-		printf("SUCCESS \n");
+		printf("FAILED \n");
 		printf("\033[0m");
 		return EXIT_FAILURE;
 	}
 }
 
+int end_insertion(int fd, char *rd_str, size_t str_sz)
+{
+	return EXIT_SUCCESS;
+}
+
 int rdwr_check(int fd, off_t offset, char *rd_str)
 {
-	return 0;
+	return EXIT_SUCCESS;
 }
 
 int main(int argc, char *argv[])
 {
 	int fd = open("test1", O_CREAT | O_RDWR | O_TRUNC, 0664);
-	char *rd_str = gen_string(10);
-
 	srand(time(NULL));
-	off_t rd_off = rand() % 500;
+	char *rd_str = gen_string(10);
 	lseek(fd, 0, SEEK_SET);
 	start_insertion(fd, rd_str, 10);
 
