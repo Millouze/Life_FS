7f5b406c3418bf1ef538155a2865c6b2003a0fd0
Florent Bordas <florentbordas18@gmail.com>
2024-05-27 00:25:40 +0200

    unit test

--- usercode/unit_test.c
+++ usercode/unit_test.c
@@ -48,15 +48,15 @@ int start_insertion(int fd, char *rd_str, size_t str_sz)
 	b = strncmp(nw_str, res_str, str_sz);
 	if ((a == str_sz) && (b == 0)) {
 		printf("START INSERTION : ");
-		printf("\033[0;32m");
+		printf(GREEN);
 		printf("SUCCESS \n");
-		printf("\033[0m");
+		printf(COLOR_DEFAULT);
 		return EXIT_SUCCESS;
 	} else {
 		printf("START INSERTION : ");
-		printf("\033[0;31m");
+		printf(RED);
 		printf("FAILED \n");
-		printf("\033[0m");
+		printf(COLOR_DEFAULT);
 		return EXIT_FAILURE;
 	}
 }
@@ -64,7 +64,7 @@ int start_insertion(int fd, char *rd_str, size_t str_sz)
 // Test pour confirmer que l'ajout en fin de fichier avec espace insuffisant echoue
 int end_insertion(int fd, char *rd_str, size_t str_sz)
 {
-	lseek(fd, FILE_MAXOFFSET - (BLK_SIZE + str_sz), SEEK_SET);
+	lseek(fd, FILE_MAXOFFSET - (BLK_SIZE * 2) - str_sz, SEEK_SET);
 	if (write(fd, rd_str, str_sz) <= 0) {
 		dprintf(STDOUT_FILENO, "Write Error : end_insertion \n");
 		return EXIT_FAILURE;
@@ -90,12 +90,43 @@ int end_insertion(int fd, char *rd_str, size_t str_sz)
 	return EXIT_SUCCESS;
 }
 
-// Test des ecritures espacees dans une fichier
+// Ecrit et verifie l'ecriture
+int write_at(int fd, off_t off, char *rd_str, size_t str_sz)
+{
+	int a = 0;
+	int b = 0;
+	char *res = malloc(str_sz);
+	memset(res, 0, str_sz);
+	lseek(fd, off, SEEK_SET);
+	if (write(fd, rd_str, str_sz) <= 0) {
+		dprintf(STDOUT_FILENO, "Write Error : end_insertion \n");
+		return EXIT_FAILURE;
+	}
+
+	lseek(fd, off, SEEK_SET);
+	a = read(fd, res, str_sz);
+	b = strncmp(rd_str, res, str_sz);
+	if (a != str_sz || b != 0) {
+		dprintf(STDOUT_FILENO, "Read Wrong Value in spaced_wr \n");
+		printf("WRITE AT : ");
+		printf(RED);
+		printf("FAILED\n");
+		printf(COLOR_DEFAULT);
+		return EXIT_FAILURE;
+	}
+
+	printf("WRITE AT : ");
+	printf(GREEN);
+	printf("SUCCESS\n");
+	printf(COLOR_DEFAULT);
+	return EXIT_SUCCESS;
+}
+
+// Test des ecritures espacees dans un fichier
 int spaced_wr(int fd, off_t off_st, off_t off_end, char *rd_str, size_t str_sz)
 {
 	int a = 0;
 	int b = 0;
-	printf("%s \n", rd_str);
 	char *res = malloc(str_sz);
 	memset(res, 0, str_sz);
 	lseek(fd, off_st, SEEK_SET);
@@ -143,7 +174,7 @@ int spaced_wr(int fd, off_t off_st, off_t off_end, char *rd_str, size_t str_sz)
 //Test des écritures au milieu d'un fichier
 int middle_insertion(int fd, char *str, size_t sz, off_t off)
 {
-	char *buf = (char *)calloc(sz,sizeof(char) );
+	char *buf = (char *)calloc(sz, sizeof(char));
 	lseek(fd, off, SEEK_SET);
 
 	if (write(fd, str, sz) <= 0) {
@@ -153,7 +184,7 @@ int middle_insertion(int fd, char *str, size_t sz, off_t off)
 	lseek(fd, off, SEEK_SET);
 	int a = read(fd, buf, sz);
 	int b = strncmp(str, buf, sz);
-	printf("a = %d \n%s, %s\n",a, str, buf);
+	printf("a = %d \n%s, %s\n", a, str, buf);
 
 	if (a != sz || b != 0) {
 		printf("MIDDLE INSERTION : ");
@@ -175,23 +206,21 @@ int middle_insertion(int fd, char *str, size_t sz, off_t off)
 
 int main(int argc, char *argv[])
 {
-	int fd = open("test1", O_CREAT | O_RDWR | O_TRUNC, 0664);
+	int fd1 = open("test1", O_CREAT | O_RDWR | O_TRUNC, 0664);
 	int fd2 = open("test2", O_CREAT | O_RDWR | O_TRUNC, 0664);
 	int fd3 = open("test3", O_CREAT | O_RDWR | O_TRUNC, 0664);
 	int fd4 = open("test4", O_CREAT | O_RDWR | O_TRUNC, 0664);
+	int fd5 = open("test5", O_CREAT | O_RDWR | O_TRUNC, 0664);
 	srand(time(NULL));
 	char *rd_str = gen_string(10);
-	start_insertion(fd, rd_str, 10);
-	end_insertion(fd2, rd_str, 10);
+	start_insertion(fd1, rd_str, 10);
+	// end_insertion(fd2, rd_str, 10);
 	spaced_wr(fd3, BLK_SIZE * 2, BLK_SIZE * 1000, rd_str, 10);
+	// write_at(fd4, FILE_MAXOFFSET - (BLK_SIZE * 3) - 10, rd_str, 10);
 	free(rd_str);
-
-	rd_str = gen_string(50);
-	write(fd4, rd_str, strlen(rd_str));
-	free(rd_str);
-	rd_str = gen_string(10);
-	middle_insertion(fd4, "aaaa", 4, 20);
-	free(rd_str);
-
+	close(fd1);
+	close(fd2);
+	close(fd3);
+	close(fd4);
 	return 0;
 }
