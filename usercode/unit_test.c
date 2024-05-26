b4852efc02db308f37edb6353838250ab4621017
Florent Bordas <florentbordas18@gmail.com>
2024-05-26 21:51:39 +0200

    fix : end_insertion

--- usercode/unit_test.c
+++ usercode/unit_test.c
@@ -64,13 +64,13 @@ int start_insertion(int fd, char *rd_str, size_t str_sz)
 // Test pour confirmer que l'ajout en fin de fichier avec espace insuffisant echoue
 int end_insertion(int fd, char *rd_str, size_t str_sz)
 {
-	lseek(fd, FILE_MAXOFFSET - str_sz, SEEK_SET);
+	lseek(fd, FILE_MAXOFFSET - (BLK_SIZE + str_sz), SEEK_SET);
 	if (write(fd, rd_str, str_sz) <= 0) {
 		dprintf(STDOUT_FILENO, "Write Error : end_insertion \n");
 		return EXIT_FAILURE;
 	}
 
-	lseek(fd, FILE_MAXOFFSET - str_sz, SEEK_SET);
+	lseek(fd, FILE_MAXOFFSET - (BLK_SIZE + str_sz), SEEK_SET);
 	if (write(fd, rd_str, str_sz) <= 0) {
 		if (errno == ENOSPC) {
 			printf("END INSERTION : ");
@@ -95,7 +95,9 @@ int spaced_wr(int fd, off_t off_st, off_t off_end, char *rd_str, size_t str_sz)
 {
 	int a = 0;
 	int b = 0;
+	printf("%s \n", rd_str);
 	char *res = malloc(str_sz);
+	memset(res, 0, str_sz);
 	lseek(fd, off_st, SEEK_SET);
 	if (write(fd, rd_str, str_sz) <= 0) {
 		dprintf(STDOUT_FILENO, "Write Error : end_insertion \n");
@@ -105,7 +107,8 @@ int spaced_wr(int fd, off_t off_st, off_t off_end, char *rd_str, size_t str_sz)
 	lseek(fd, off_st, SEEK_SET);
 	a = read(fd, res, str_sz);
 	b = strncmp(rd_str, res, str_sz);
-	if (a == str_sz || b != 0) {
+	if (a != str_sz || b != 0) {
+		printf("res : %s %d %d\n", res, b, a);
 		dprintf(STDOUT_FILENO, "Read Wrong Value in spaced_wr \n");
 		return EXIT_FAILURE;
 	}
@@ -120,7 +123,7 @@ int spaced_wr(int fd, off_t off_st, off_t off_end, char *rd_str, size_t str_sz)
 	lseek(fd, off_end, SEEK_SET);
 	a = read(fd, res, str_sz);
 	b = strncmp(nw_str, res, str_sz);
-	if (a == str_sz || b != 0) {
+	if (a != str_sz || b != 0) {
 		printf("SPACED WRITE : ");
 		printf(RED);
 		printf("FAILED \n");
@@ -141,11 +144,12 @@ int main(int argc, char *argv[])
 {
 	int fd = open("test1", O_CREAT | O_RDWR | O_TRUNC, 0664);
 	int fd2 = open("test2", O_CREAT | O_RDWR | O_TRUNC, 0664);
+	int fd3 = open("test3", O_CREAT | O_RDWR | O_TRUNC, 0664);
 	srand(time(NULL));
 	char *rd_str = gen_string(10);
 	start_insertion(fd, rd_str, 10);
 	end_insertion(fd2, rd_str, 10);
-	spaced_wr(fd, BLK_SIZE * 2, BLK_SIZE * 1000, rd_str, 10);
+	spaced_wr(fd3, BLK_SIZE * 2, BLK_SIZE * 1000, rd_str, 10);
 	free(rd_str);
 
 	return 0;
